863fe6866f6b94970ad97c3e5dbf943a3d2b2a61
Haroon <Haroon.a.kh@gmail.com>
2013-09-05 22:12:24 -0700

    Bonus

---
+++ hello.java
@@ -0,0 +1,23 @@
+import java.io.* ;
+
+class hello
+{  
+        public static void main(String [] args)
+        {
+
+	  BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+
+           System.out.println("Enter your name: ");
+		
+	try {
+
+	   String name = in.readLine();
+
+	   System.out.println("Hello " + name);	
+	     }
+
+	catch (IOException err) {
+		System.out.println("Error");
+        }
+}
+}
